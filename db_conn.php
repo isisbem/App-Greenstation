027b637e9afa40863843b27dc481bde7e89e63b4
Matty <74145047+Matteo0099@users.noreply.github.com>
2024-02-26 09:05:20 +0100

    fix the base template connection

--- db_conn.php
+++ db_conn.php
@@ -7,6 +7,4 @@ $myDB = 'green_station';
 $mysqli = new mysqli($host, $username, $psw, $myDB);
 if ($mysqli->connect_error) {
   die('Errore di connessione (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-} else {
-  echo 'Connesso. ' . $mysqli->host_info . "\n";
 }
