3013942b8d57578f2b603abd73a27c27944cfdaa
Matty <74145047+Matteo0099@users.noreply.github.com>
2024-03-26 08:30:16 +0100

    fix database.sql
    date added like parameters

--- data/database.sql
+++ data/database.sql
@@ -3,11 +3,11 @@
 -- una volta collegata l'app al server sftp, questo file è solo per local development
 
 CREATE TABLE `green_station`.`rilevamenti` (
-  `id` INT(50) NOT NULL,
-  `prese_occupate` INT(200) NOT NULL,
-  `consumo_istantaneo` INT(200) NOT NULL,
-  `consumo_totale` INT(200) NOT NULL,
-  `data_ora_inizio_carica` INT(100) NOT NULL,
-  `data_ora_fine_carica` INT(100) NOT NULL,
+  `id` INT(100) NOT NULL,
+  `prese_occupate` INT(100) NOT NULL,
+  `consumo_istantaneo` INT(100) NOT NULL,
+  `consumo_totale` INT(100) NOT NULL,
+  `data_ora_inizio_carica` TIME(6) NOT NULL,
+  `data_ora_fine_carica` TIME(6) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
