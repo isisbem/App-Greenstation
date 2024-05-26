b2df7040ecef5b10fa69b433af2be3169b7af709
Matteo Manià <matteomania05@gmail.com>
2024-05-26 11:21:53 +0200

    update and js/php fixed + sql

--- data/database.sql
+++ data/database.sql
@@ -1,6 +1,8 @@
 -- questo è un database di prova
 -- serve per verificare il corretto funzionamento dell'applicazione in corso di sviluppo
 -- una volta collegata l'app al server sftp, questo file è solo per local development
+-- database: 'green_station'
+-- table: 'rilevamenti'
 
 CREATE TABLE `green_station`.`rilevamenti` (
   `id` INT(100) NOT NULL,
--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
+// Gestione stato dinamico delle prese
 document.addEventListener("DOMContentLoaded", function () {
-  // Update dell'anno
+  // Funzione per aggiornare lo stato delle prese
+  function updateStates() {
+    const realTimeStates = document.querySelectorAll(".stato");
+    let svg = document.querySelectorAll("svg.icon");
+    svg.forEach(function (svgs, index) {
+      if (svgs.classList.contains("text-green-600")) realTimeStates[index].innerHTML = 'Libera!';
+      else realTimeStates[index].innerHTML = 'Occupata!';
+    });
+  }
+  // Aggiornamento iniziale dello stato delle prese
+  updateStates();
+
+  // Aggiornamento prese ogni (5000ms = 5s secondi)
+  setInterval(function () {
+    updateStates();
+  }, 5000);
+
+  // Aggiornamento dell'anno(footer)
   let year = document.querySelectorAll(".year");
   let date = new Date();
   let thisY = date.getFullYear();
-  year.innerHTML = thisY;
-
-  // stato, index
-  const realTimeStates = document.querySelectorAll(".stato");
-  let svg = document.querySelectorAll("svg.icon");
-  svg.forEach(function (svgs, index) {
-    svgs.classList.contains("text-green-600") 
-    ? realTimeStates[index].innerHTML = 'Libera!'
-    : realTimeStates[index].innerHTML = 'Occupata!';
-  })
-})
+  year.forEach(function (element) {
+    element.innerHTML = thisY;
+  });
+});
